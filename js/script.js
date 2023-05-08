1ea82bd4cf8ee0635275c7a67c028989ef4cd84c
aryawirap <aryawirap2002@gmail.com>
2023-05-09 05:51:20 +0800

    membuat website kopi

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika kopi menu di klik
+document.querySelector("#kopi-menu"),
+  (onclick = () => {
+    navbarNav.classList.toggle("active");
+  });
